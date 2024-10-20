3f95a3bfa22c667582b41b73cb39aa4771fde55b
kienhoang3397 <125807253+kienhoang3397@users.noreply.github.com>
2024-10-21 01:08:17 +0700

    feat: ui dashboard

---
+++ src/layouts/DBLayout.tsx
@@ -0,0 +1,39 @@
+import React, { useState, ReactNode } from 'react';
+
+import SideBarDashBoard from '@/containers/DashBoardPage/SideBarDashBoard';
+import HeaderDB from '@/containers/DashBoardPage/HeaderDB';
+
+
+const DBLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="dark:bg-boxdark-2 dark:text-bodydark">
+      {/* <!-- ===== Page Wrapper Start ===== --> */}
+      <div className="flex h-screen overflow-hidden">
+        {/* <!-- ===== Sidebar Start ===== --> */}
+        <SideBarDashBoard sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        {/* <!-- ===== Sidebar End ===== --> */}
+
+        {/* <!-- ===== Content Area Start ===== --> */}
+        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+          {/* <!-- ===== Header Start ===== --> */}
+          <HeaderDB sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          {/* <!-- ===== Header End ===== --> */}
+
+          {/* <!-- ===== Main Content Start ===== --> */}
+          <main>
+            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+              {children}
+            </div>
+          </main>
+          {/* <!-- ===== Main Content End ===== --> */}
+        </div>
+        {/* <!-- ===== Content Area End ===== --> */}
+      </div>
+      {/* <!-- ===== Page Wrapper End ===== --> */}
+    </div>
+  );
+};
+
+export default DBLayout;
