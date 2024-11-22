56a43c2e0f33263514cc929cb6256d75145b24c9
kienhoang3397 <125807253+kienhoang3397@users.noreply.github.com>
2024-11-23 03:08:53 +0700

    feat: update dashboard routes and components for improved organization and functionality

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -3,7 +3,7 @@
 import LandingLayout from "../layouts/LandingLayout";
 import Home from "../pages/Home";
 
-import DBLayout from "@/layouts/DBLayout";
+
 import MainDashBoardPage from "@/pages/DashBoard/MainDashBoard";
 import AuthLayout from "../layouts/AuthLayout";
 import AuthPage from "../pages/Auth";
@@ -19,18 +19,29 @@ import Profile from "../pages/Profile"
 import EditProfile from '../pages/EditProfile';
 
 
-import { AddRole } from "@/pages/DashBoard/AddRolePage";
+// import { AddRole } from "@/pages/DashBoard/AddRolePage";
 import RoleDB from "@/pages/DashBoard/RolePage";
 
-import EditUser from "@/containers/DashBoardPage/UsersDB/EditUser";
+// import EditUser from "@/containers/DashBoardPage/UsersDB/EditUser";
 import UsersDB from "@/pages/DashBoard/UsersDB";
 
 
 import ChangePassword from "@/containers/User/ChangePassword";
-import DropdownNavbarComponent from "@/containers/User/NavUser";
-import UserSettings from "@/containers/User/UserSettings";
+// import DropdownNavbarComponent from "@/containers/User/NavUser";
+// import UserSettings from "@/containers/User/UserSettings";
 import ChatPage from "@/pages/ChatPage";
 import VideoCallPage from "@/pages/VideoCallPage";
+import { DashboardLayout } from "@/layouts/DBLayout";
+import { UsersDBPage } from "@/pages/DashBoard/UsersDBPage";
+import { RolesDBPage } from "@/pages/DashBoard/RoleDBPage";
+import { PermissionsDBPage } from "@/pages/DashBoard/PermissionsDBPage";
+import { ReactionTypesPage } from "@/pages/DashBoard/ReactionTypesPage";
+import { EditUserDBPage } from "@/pages/DashBoard/EditUserPage";
+import { EditRolePage } from "@/pages/DashBoard/EditRoleDBPage";
+import { Edit } from "lucide-react";
+import { EditPermissionPage } from "@/pages/DashBoard/EditPermissionsDBPage";
+import HomeDBPage from "@/pages/DashBoard/HomeDBPage";
+
 interface Route {
     path: string;
     component: React.ComponentType<any>;
@@ -98,62 +109,98 @@ const publicRoutes: Route[] = [
         layout: AuthLayout,
         params: { token: ':token' },
     },
-    {
-        path: "/dashboard",
-        component: MainDashBoardPage,
-        layout: DBLayout,
-    },
-    {
-        path: "/dashboard/role",
-        component: RoleDB,
-        layout: DBLayout,
-    },
+    // {
+    //     path: "/dashboard",
+    //     component: MainDashBoardPage,
+    //     layout: DashboardLayout,
+    // },
+    // {
+    //     path: "/dashboard/role",
+    //     component: RoleDB,
+    //     layout: DashboardLayout,
+    // },
     // {
 
     //     path: "/dashboard/permission",
     //     component: Permission,
-    //     layout: DBLayout,
+    //     layout: DashboardLayout,
     // },
     // {
     //     path: "/dashboard/permission/addpermission",
     //     component: AddPermission,
-    //     layout: DBLayout,
+    //     layout: DashboardLayout,
     // },
     // {
     //     path: "/dashboard/permission/updatepermission/:id",
     //     component: UpdatePermission,
-    //     layout: DBLayout,
+    //     layout: DashboardLayout,
+    // },
+    // {
+    //     path: "/dashboard/role/add",
+    //     component: AddRole,
+    //     layout: DashboardLayout,
     // },
     {
-        path: "/dashboard/role/add",
-        component: AddRole,
-        layout: DBLayout,
-    },
-    {
         path: "/dashboard/users",
-        component: UsersDB,
-        layout: DBLayout,
+        component: UsersDBPage,
+        layout: DashboardLayout,
+    },
+    {
+        path: "/dashboard/users/edit/:id",
+        component: EditUserDBPage,
+        layout: DashboardLayout,
     },
 
     {
-        path: "/dashboard/users/:userId",
-        component: EditUser,
-        layout: DBLayout,
+        path: "/dashboard/roles",
+        component: RolesDBPage,
+        layout: DashboardLayout,
+    },
+    {
+        path: "/dashboard/roles/edit/:id",
+        component: EditRolePage,
+        layout: DashboardLayout,
+    },
+    {
+        path: "/dashboard/permissions",
+        component: PermissionsDBPage,
+        layout: DashboardLayout,
+    },
+    {
+        path: "/dashboard/permissions/edit/:id",
+        component: EditPermissionPage,
+        layout: DashboardLayout,
+    },
+    {
+        path: "/dashboard/reaction-types",
+        component: ReactionTypesPage,
+        layout: DashboardLayout,
+    },
+    {
+        path: "/dashboard",
+        component: HomeDBPage,
+        layout: DashboardLayout,
     },
 
-    {
-        path: "/nav-home",
-        component: DropdownNavbarComponent,
-    },
-    {
-        path: "/settings/change-password",
-        component: ChangePassword,
-    },
+    // {
+    //     path: "/dashboard/users/:userId",
+    //     component: EditUser,
+    //     layout: DashboardLayout,
+    // },
 
-    {
-        path: "/settings",
-        component: UserSettings,
-    },
+    // {
+    //     path: "/nav-home",
+    //     component: DropdownNavbarComponent,
+    // },
+    // {
+    //     path: "/settings/change-password",
+    //     component: ChangePassword,
+    // },
+
+    // {
+    //     path: "/settings",
+    //     component: UserSettings,
+    // },
 
 
 
